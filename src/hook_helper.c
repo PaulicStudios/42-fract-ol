67c4c5fe3ac799c8b871fe68785020f34fe65bcb
pgrossma <pgrossma@student.42heilbronn.de>
2023-11-14 13:59:02 +0100

    finished?

--- src/hook_helper.c
+++ src/hook_helper.c
@@ -6,7 +6,7 @@
 /*   By: pgrossma <pgrossma@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 20:29:43 by pgrossma          #+#    #+#             */
-/*   Updated: 2023/11/14 13:51:26 by pgrossma         ###   ########.fr       */
+/*   Updated: 2023/11/14 13:57:47 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,13 +48,13 @@ bool	ft_key_change_values(mlx_key_data_t keydata, t_win *win)
 	else if (keydata.key == MLX_KEY_4)
 		ft_setup_fern(win);
 	else if (keydata.key == MLX_KEY_MINUS)
-		win->fract->iter_max += 2;
-	else if (keydata.key == MLX_KEY_EQUAL)
 		win->fract->iter_max -= 2;
+	else if (keydata.key == MLX_KEY_EQUAL)
+		win->fract->iter_max += 2;
 	else if (keydata.key == MLX_KEY_LEFT_BRACKET)
-		win->fract->limit += 0.1;
-	else if (keydata.key == MLX_KEY_RIGHT_BRACKET)
 		win->fract->limit -= 0.1;
+	else if (keydata.key == MLX_KEY_RIGHT_BRACKET)
+		win->fract->limit += 0.1;
 	else
 		return (false);
 	return (true);
