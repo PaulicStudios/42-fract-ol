b0eb3f6be07a39805416ecf601653c96357459c7
pgrossma <pgrossma@student.42heilbronn.de>
2023-11-24 17:53:23 +0100

    added more iterations to fern on keypress

--- src/hook_helper.c
+++ src/hook_helper.c
@@ -6,7 +6,7 @@
 /*   By: pgrossma <pgrossma@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 20:29:43 by pgrossma          #+#    #+#             */
-/*   Updated: 2023/11/24 17:46:45 by pgrossma         ###   ########.fr       */
+/*   Updated: 2023/11/24 17:51:31 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,9 +48,17 @@ bool	ft_key_change_values(mlx_key_data_t keydata, t_win *win)
 	else if (keydata.key == MLX_KEY_4)
 		ft_setup_fern(win);
 	else if (keydata.key == MLX_KEY_MINUS)
+	{
+		if (win->fract->type == FERN)
+			win->fract->iter_max -= 1000;
 		win->fract->iter_max -= 2;
+	}
 	else if (keydata.key == MLX_KEY_EQUAL)
+	{
+		if (win->fract->type == FERN)
+			win->fract->iter_max += 1000;
 		win->fract->iter_max += 2;
+	}
 	else if (keydata.key == MLX_KEY_LEFT_BRACKET)
 		win->fract->limit -= 0.1;
 	else if (keydata.key == MLX_KEY_RIGHT_BRACKET)
